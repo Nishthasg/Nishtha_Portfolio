218135059ec175ca863ed630957a8339792db56a
Nishtha Singh <nishthasingh116@gmail.com>
2025-11-10 21:44:06 +0530

    10/11/25

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SkillsComponent } from './pages/skills/skills.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent],
+  imports: [RouterOutlet,NavbarComponent,SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
+  downloadResume(){
+    window.open('Nishtha_Singh_Resume.pdf','_blank');
+  }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+ isScrolled=false;
+ isSidebarOpen = false;
+  @HostListener('window:scroll', [])
+   onWindowScroll() {
+    if (window.innerWidth > 768) {
+      this.isScrolled = window.scrollY > 50;
+    } else {
+      this.isScrolled = false;
+    }
+  }
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
