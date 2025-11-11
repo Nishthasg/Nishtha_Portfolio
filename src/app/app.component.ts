a9a0784e51f9c9f9222528d7293c4306e23bbce3
Nishtha Singh <nishthasingh116@gmail.com>
2025-11-11 14:40:00 +0530

    11/11/25

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SkillsComponent } from './pages/skills/skills.component';
+import { EducationComponent } from './pages/education/education.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,SkillsComponent],
+  imports: [RouterOutlet,NavbarComponent,SkillsComponent, EducationComponent, ProjectsComponent, ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
